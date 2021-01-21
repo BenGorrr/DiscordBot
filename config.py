5d6edb58616a9db0eb48cbb9733b712aed2897bf
BenGorrr <ggbengaben@gmail.com>
2021-01-21 22:39:33 +0800

    updated config

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import psycopg2
 # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
 
 # DATABASE_URI = 'postgres+psycopg2://postgres:76541@localhost:5432/classes'
-DATABASE_URI = environ.get('DATABASE_URI')
+DATABASE_URL = environ.get('DATABASE_URL')
 
 conn = psycopg2.connect(DATABASE_URI, sslmode='require')
