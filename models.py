bc4e8097c1a95220228f88815764a139c0ca2242
BenGorrr <ggbengaben@gmail.com>
2021-01-21 21:51:58 +0800

    Added course code

--- models.py
+++ models.py
@@ -6,9 +6,10 @@ Base = declarative_base()
 class Classes(Base):
     __tablename__ = 'classes'
     id = Column(Integer, primary_key=True)
+    course_code = Column(String)
     course_name = Column(String)
     link = Column(String)
 
     def __repr__(self):
-        return "<Classes(course_name='{}', link='{}')"\
-        .format(self.course_name, self.link)
+        return "<Classes({} course_code='{}', course_name='{}', link='{}')"\
+        .format(self.id, self.course_code, self.course_name, self.link)
