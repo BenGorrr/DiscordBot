f23aec9c859319d9747debf0afb5738e26083410
BenGorrr <ggbengaben@gmail.com>
2021-02-04 08:53:14 +0800

    Updated config to production

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ import psycopg2
 # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
 
 #Development
-from os.path import join, dirname
-from dotenv import load_dotenv
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-DATABASE_URL = 'postgres+psycopg2://postgres:76541@localhost:5432/classes'
-conn = DATABASE_URL
+# from os.path import join, dirname
+# from dotenv import load_dotenv
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
+# DATABASE_URL = 'postgres+psycopg2://postgres:76541@localhost:5432/classes'
+# conn = DATABASE_URL
 
 #Production
-# DATABASE_URL = environ.get('DATABASE_URL')
-# conn = DATABASE_URL
+DATABASE_URL = environ.get('DATABASE_URL')
+conn = DATABASE_URL
