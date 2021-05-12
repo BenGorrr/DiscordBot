a1ecfda2191e152662df33383d0dacea054fc146
BenGorrr <ggbengaben@gmail.com>
2021-05-12 23:02:30 +0800

    Added ImagesClient class

--- Images/images.py
+++ Images/images.py
@@ -1,28 +1,55 @@
 import os, requests, json, discord
 from discord.ext import commands
 
-class Images(commands.Cog):
+class ImagesCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.client = ImagesClient()
 
     @commands.command()
     async def image(self, ctx, tag=None):
         print(tag)
         if tag != None:
-            img = self.get_random_image(tag)
-            await ctx.send(img)
+            img = self.client.get_random_image(tag)
+            if img != None:
+                await ctx.send(img)
+            else: await ctx.send("Can't find any :c")
         else: await ctx.send("usage: .image tag_name")
 
-    def get_random_image(self, tag):
-        url = "https://api.ksoft.si/images/random-image?tag=" + tag
-        headers = {'Authorization': 'Bearer ' + os.environ.get('KSOFT_KEY', '-1')}
-        resp = requests.get(url, headers=headers)
-        content = json.loads(resp.text)
-        #print(content)
-        if(resp.status_code != 200):
-            print(content['message'])
-            return
-        return content["url"]
+
+class ImagesClient():
+    def __init__(self, base_url=''):
+        self.set_base_url(base_url)
+        self.api_key = os.environ.get('KSOFT_KEY', '-1')
+
+    # set base url, if none are given, use default url
+    def set_base_url(self, base_url):
+        if base_url == '':
+            base_url = "https://api.ksoft.si/images"
+        self.base_url = base_url.rstrip('/')
+
+    # send request and return it if status 200
+    def do(self, method, path, req=None):
+        try :
+            params = json.loads(json.dumps(req))
+        except Exception:
+            params = None
+        headers = {'Authorization': 'Bearer ' + self.api_key}
+        args = dict(params=params, headers=headers)
+
+        url = self.base_url + path
+        resp = requests.request(method, url, **args)
+
+        if resp.status_code == 200:
+            return resp.json()
+        else: print("ERROR: " + resp.json()['message'])
+
+    def get_random_image(self, tag, nsfw=False):
+        req = { 'tag':tag , 'nsfw':nsfw }
+        resp = self.do('GET', '/random-image', req)
+
+        if resp != None:
+            return resp['url']
 
 def setup(bot):
-    bot.add_cog(Images(bot))
+    bot.add_cog(ImagesCog(bot))
