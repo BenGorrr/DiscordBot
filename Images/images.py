31383a5582b4006786073469d3e0805e88c735c7
BenGorrr <ggbengaben@gmail.com>
2021-05-12 23:30:26 +0800

    Added nsfw var with command, added getimagetags

--- Images/images.py
+++ Images/images.py
@@ -5,17 +5,32 @@ class ImagesCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.client = ImagesClient()
+        self.nsfw = False
+
+    @commands.command()
+    async def imageNSFW(self, ctx, state=""):
+        if state.lower() in ["yes", "no"]:
+            self.nsfw = True if state.lower() == "yes" else False
+            await ctx.send("Image NSFW: " + str(self.nsfw))
+        else: await ctx.send("usage: .imageNSFW (yes | no)")
 
     @commands.command()
     async def image(self, ctx, tag=None):
-        print(tag)
         if tag != None:
-            img = self.client.get_random_image(tag)
+            img = self.client.get_random_image(tag, self.nsfw)
             if img != None:
                 await ctx.send(img)
             else: await ctx.send("Can't find any :c")
         else: await ctx.send("usage: .image tag_name")
 
+    @commands.command()
+    async def imagetags(self, ctx):
+        tags = self.client.get_tags_list()
+        if tags != None:
+            normal_tags = ', '.join(tags['tags'])
+            nsfw_tags = ', '.join(tags['nsfw_tags'])
+            await ctx.send("Tags: " + normal_tags + "\nNSFW Tags: " + nsfw_tags)
+        else: await ctx.send("Can't find any :c")
 
 class ImagesClient():
     def __init__(self, base_url=''):
@@ -44,12 +59,18 @@ class ImagesClient():
             return resp.json()
         else: print("ERROR: " + resp.json()['message'])
 
-    def get_random_image(self, tag, nsfw=False):
+    def get_random_image(self, tag, nsfw):
         req = { 'tag':tag , 'nsfw':nsfw }
         resp = self.do('GET', '/random-image', req)
 
         if resp != None:
             return resp['url']
+
+    def get_tags_list(self):
+        resp = self.do('GET', '/tags')
+
+        if resp != None:
+            return resp
 
 def setup(bot):
     bot.add_cog(ImagesCog(bot))
