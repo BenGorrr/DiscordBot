d90de12c955a23ece2f5076c1b1c2fc3d5756d03
BenGorrr <ggbengaben@gmail.com>
2022-01-07 12:20:16 +0800

    Minor header fix

--- ValorantAPI/valorant.py
+++ ValorantAPI/valorant.py
@@ -201,8 +201,7 @@ class Client():
             params = json.loads(json.dumps(req))
         except Exception:
             params = None
-        headers = {}
-        headers = {"Authorization": "Bearer " + self.access_token, "X-Riot-Entitlements-JWT": self.entitlements_token} if auth else None
+        headers = {"Authorization": "Bearer " + self.access_token, "X-Riot-Entitlements-JWT": self.entitlements_token} if auth else {}
         headers['Accept'] = 'application/json'
         headers['Content-Type'] = 'application/json'
         
